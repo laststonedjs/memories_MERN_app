6edd24fffb68fa4e745da84f8d103e49952d615d
Filip <filipsarovic428@gmail.com>
2023-03-30 17:05:04 +0200

    Completed initialize Redux in the App

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// redux
+import { Provider } from "react-redux";
+// import { configureStore } from "@reduxjs/toolkit";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+// reducers
+import reducers from "./reducers";
 
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
---
+++ client/src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux";
+// posts
+import posts from "./posts";
+
+export default combineReducers({ posts });
